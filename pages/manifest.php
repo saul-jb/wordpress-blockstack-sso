7b846bc3a6b22d5de6911b88bb89b61dc0bcc7a4
Aranad <aran@organicdesign.co.nz>
2018-04-20 17:01:41 -0300

    move manifest into php

--- pages/manifest.php
+++ pages/manifest.php
@@ -14,7 +14,7 @@ header( 'Access-Control-Allow-Headers: "Content-Type"' );
 {
 	"name": "Wordpress Blockstack Log-in",
 	"start_url": "<?php echo $_SERVER['SERVER_NAME']; ?>",
-	"description": "The blockstack plugin to log into wordpress with blockstack",
+	"description": "The blockstack plugin to log into wordpress with blockstack (test)",
 	"icons": [
 		{
 			"src": "https://blockstack.org/images/logos/blockstack-bug.svg",
